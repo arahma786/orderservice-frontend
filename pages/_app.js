89ede4a7be393718fdda18b7efd77e6501c69e54
arahma <arahma@iu.edu>
2023-04-28 23:22:15 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import Layout from '@/components/Layout'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+      <Html lang="en">
+        <Head />
+        <body>
+        <Main />
+        <NextScript />
+        </body>
+      </Html>
+  )
 }
